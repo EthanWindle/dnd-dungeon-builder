1e5d38e420aa80ca5a00e3fc021935f54b724014
Braeden Windle <windlebrae@ecs.vuw.ac.nz>
2023-08-15 22:11:47 +1200

    Add generation of walls to GridController

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -18,6 +18,7 @@ public class GridController : MonoBehaviour{
     private GameObject[,] foregroundLayer;
 
     public GameObject[] rooms;
+    public GameObject wallTile;
 
     public void Awake()
     {
@@ -31,6 +32,29 @@ public class GridController : MonoBehaviour{
             room.GetComponent<RoomController>().PlaceRoom(gameObject.transform, backgroundLayer, foregroundLayer, cellSize, cellSpacing);
         }
 
+        List<Vector2> wallLocations = new List<Vector2>();
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if (shouldBeWall(x, y))
+                {
+                    wallLocations.Add(new Vector2(x, y));
+                }
+            }
+        }
+
+        foreach (Vector2 wallLocation in wallLocations)
+        {
+            int x = (int)wallLocation.x;
+            int y = (int)wallLocation.y;
+            GameObject wall = Instantiate(wallTile, new Vector3(x * (cellSize + cellSpacing), y * (cellSize + cellSpacing), 1), Quaternion.identity, gameObject.transform);
+            wall.GetComponent<TileController>().Init(cellSize - cellSpacing * 2);
+            backgroundLayer[x, y] = wall;
+        }
+
         gameObject.transform.position -= new Vector3(width * cellSize / 2, width * cellSize / 2, 0);
     }
 
@@ -40,6 +64,25 @@ public class GridController : MonoBehaviour{
     private Vector2 GetWorldLocation(int x, int y)
     {
         return new Vector2(x, y) * cellSize;
+    }
+
+
+    private bool shouldBeWall(int x, int y)
+    {
+
+        if (backgroundLayer[x, y] != null) return false;
+
+        for (int xi = x-1; xi <= x+1; xi++)
+        {
+            if (xi < 0 || xi >= width) continue;
+            for (int yi = y-1; yi <= y+1; yi++)
+            {
+                if (yi < 0 || yi >= height || (yi == y && xi == x)) continue;
+                if (backgroundLayer[xi, yi] != null) return true;
+            }
+        }
+
+        return false;
     }
 
 }
