c642423bfed1348d47989c95760626ab51ebb270
Braeden Windle <windlebrae@ecs.vuw.ac.nz>
2023-08-15 13:44:56 +1200

    Remove Tile.cs, create TileController.cs
    From what I can tell, unity scripts tend to be called BlahController, while the object itself will be called Blah. For our use case, I have changed Tile.cs to TileController.cs and also made the script extend from MonoBehaviour - this is because scripts need to extend from MonoBehaviour in order to be attached to a GameObject. Currently, TileController has a method for resizing the tile, as the location of the tile can be set when Instantiating it within the GridController

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+
+    public void Init(float size)
+    {
+        Transform transform = gameObject.GetComponent<Transform>();
+        transform.localScale = new Vector3(size,size,1);
+      
+    }
+}
