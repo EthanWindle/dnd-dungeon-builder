0754161bf32c9a000012d89ee75048c041b9c391
Adam Roddick <roddicadam@ecs.vuw.ac.nz>
2023-09-06 11:53:14 +1200

    Added imports to Recorder.cs

--- Assets/Scripts/Recorder/Recorder.cs
+++ Assets/Scripts/Recorder/Recorder.cs
@@ -1,6 +1,10 @@
+using System;
+using Newtonsoft.Json;
+using System.IO;
+
 public class Recorder
 {
-    public static void saveGame()
+    public static void saveGame(GridController map, String fileName)
     {
 
     }
