fe2d27079f09c512e513229cc6fc78195e10ff7d
windleetha <windleetha@ecs.vuw.ac.nz>
2023-09-12 22:04:14 +1200

    text now disappears before animation, and animation run when both generate map and load map buttons are pressed and exit animationa added

--- Assets/ButtonControls.cs
+++ Assets/ButtonControls.cs
@@ -6,23 +6,36 @@ using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Animator flipPage;
+    public GameObject text;
     
     //Home page Buttons
     public void GenerateNewMap(){
         StartCoroutine(generateMap());  
     }
-
     IEnumerator generateMap() {
+        text.SetActive(false);
         flipPage.SetTrigger("LeftFlip");
         yield return new WaitForSeconds(1); 
         SceneManager.LoadScene("settingPage");
     }
     public void LoadMap(){
+        StartCoroutine(load());
+    }
+    IEnumerator load() {
+        text.SetActive(false);
+        flipPage.SetTrigger("LeftFlip");
+        yield return new WaitForSeconds(1); 
         SceneManager.LoadScene("loadPage");
     }
 
     public void Exit(){
-
+        StartCoroutine(exit());
+    }
+    IEnumerator exit() {
+        text.SetActive(false);
+        flipPage.SetTrigger("Exit");
+        yield return new WaitForSeconds(1); 
+        Application.Quit();
     }
 
 
