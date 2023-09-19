d722690a531a70435f2f15c1ec56f2efe4e49ea7
Adam Roddick <roddicadam@ecs.vuw.ac.nz>
2023-09-20 11:16:46 +1200

    Added variables cellSize and cellSpacing to Recorder.cs

--- Assets/Scripts/Recorder/Recorder.cs
+++ Assets/Scripts/Recorder/Recorder.cs
@@ -8,12 +8,23 @@ public class Recorder
     public int width;
     public int height;
     // Add other serializable fields here
+    public float cellSize;
+    public float cellSpacing;
+
+    //private GameObject[,] backgroundLayer; //Layer for tiles like walls, floors, doors.
+    //private GameObject[,] foregroundLayer; //Layer for props and entities like players and monsters
+
+    public GameObject[] rooms;
 
     public Recorder(GridController gridController)
     {
         width = gridController.width;
         height = gridController.height;
         // Initialize other serializable fields here
+        cellSize = gridController.cellSize;
+        cellSpacing = gridController.cellSpacing;
+        rooms = gridController.rooms;
+        
     }
 }
 
@@ -25,6 +36,5 @@ public class GridControllerJsonSerializer : MonoBehaviour
         
         string json = JsonUtility.ToJson(serializableGridController, true);
         File.WriteAllText(filePath, json);
-        Debug.Log("pee");
     }
 }
