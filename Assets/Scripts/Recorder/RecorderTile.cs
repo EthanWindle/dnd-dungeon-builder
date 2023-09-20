19d9a8ba894c727d2378ab021ee7f2b8f0beaf86
Adam Roddick <roddicadam@ecs.vuw.ac.nz>
2023-09-20 14:11:51 +1200

    Create RecorderTile.cs class for recorder to save tiles

---
+++ Assets/Scripts/Recorder/RecorderTile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[System.Serializable]
+public class RecorderTile
+{
+    public String type;
+    public int x;
+    public int y;
+
+    public RecorderTile(String type, int x, int y)
+    {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+}
