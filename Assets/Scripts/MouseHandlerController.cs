80b841e8f8cfa9a459a3826f115a62c070943fea
Braeden Windle <windlebrae@ecs.vuw.ac.nz>
2023-09-21 11:28:00 +1200

    Update MouseControllerHandler to use Grid bounds

--- Assets/Scripts/MouseHandlerController.cs
+++ Assets/Scripts/MouseHandlerController.cs
@@ -8,12 +8,22 @@ public class MouseHandlerController : MonoBehaviour
     Vector3 mousePosition;
     bool mouseDown;
 
+    float cameraExtendVertical;
+
+    float cameraExtendHorizontal;
+
+    float topBound, leftBound, bottomBound, rightBound;
+
+
     GameObject cameraObject;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraObject = GameObject.Find("Main Camera");
+        cameraExtendVertical = cameraObject.GetComponent<Camera>().orthographicSize;
+        cameraExtendHorizontal = (cameraExtendVertical * Screen.width) / Screen.height;
+        gameObject.GetComponent<GridController>().getGridBounds(out topBound, out leftBound, out bottomBound, out rightBound);
     }
 
     // Update is called once per frame
@@ -33,6 +43,9 @@ public class MouseHandlerController : MonoBehaviour
                 Vector3 distance = currentMousePos - mousePosition;
                 Vector3 cameraPosition = cameraObject.transform.position;
                 cameraPosition -= distance;
+
+                cameraPosition.x = Mathf.Min(rightBound, Mathf.Max(leftBound, cameraPosition.x));
+                cameraPosition.y = Mathf.Min(topBound, Mathf.Max(bottomBound, cameraPosition.y));
 
                 cameraObject.transform.position = cameraPosition;
             }
