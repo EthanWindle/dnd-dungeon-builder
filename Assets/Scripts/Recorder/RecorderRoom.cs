dc41b40ae98282d59efd977182206ca27ed29fa5
Adam Roddick <roddicadam@ecs.vuw.ac.nz>
2023-09-20 20:51:11 +1200

    Create RecorderRoom.cs object, similar to RecorderTile but stores information for each room

---
+++ Assets/Scripts/Recorder/RecorderRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[System.Serializable]
+public class RecorderRoom
+{
+    public int RoomNumber;
+    public int XPos;
+    public int YPos;
+
+    public RecorderRoom(int roomNumber, int x, int y)
+    {
+        this.RoomNumber = roomNumber;
+        this.XPos = x;
+        this.YPos = y;
+    }
+}
