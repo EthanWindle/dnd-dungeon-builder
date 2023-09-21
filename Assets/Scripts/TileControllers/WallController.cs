b29214b03123e5521344b0b07f12b04fff5d7f11
Braeden Windle <windlebrae@ecs.vuw.ac.nz>
2023-09-21 23:55:21 +1200

    Implement top and bottom walls for WallController

--- Assets/Scripts/TileControllers/WallController.cs
+++ Assets/Scripts/TileControllers/WallController.cs
@@ -13,28 +13,38 @@ public class WallController : TileController
 
     public Texture2D rightWall;
     public Texture2D leftWall;
+    public Texture2D topWall;
+    public Texture2D bottomWall;
 
     public override bool CanEnter()
     {
         return false;
     }
     //Takes an array of adjacent tiles, of length eight in the format:
-    //0 3 5
-    //1 x 6
     //2 4 7
+    //1 x 6
+    //0 3 5
     public void SetTexture(TileController[] adjacentTiles)
     {
         Texture2D texture;
 
         //Is a right wall
-        if (adjacentTiles[1] is FloorController &&  (adjacentTiles[0] is FloorController ||  adjacentTiles[2] is FloorController))
+        if (adjacentTiles[1] is FloorController && (adjacentTiles[0] is FloorController || adjacentTiles[2] is FloorController))
         {
             texture = rightWall;
         }
         //Is a right wall
-        else if (adjacentTiles[6] is FloorController && (adjacentTiles[5] is FloorController ||  adjacentTiles[7] is FloorController))
+        else if (adjacentTiles[6] is FloorController && (adjacentTiles[5] is FloorController || adjacentTiles[7] is FloorController))
         {
             texture = leftWall;
+        }
+        else if (adjacentTiles[4] is FloorController && (adjacentTiles[2] is FloorController || adjacentTiles[7] is FloorController))
+        {
+            texture = bottomWall;
+        }
+        else if (adjacentTiles[3] is FloorController && (adjacentTiles[0] is FloorController || adjacentTiles[5] is FloorController))
+        {
+            texture = topWall;
         }
 
         else
