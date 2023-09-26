dedd61f8e07d2c7ec93df621242ddc6c611cc6bd
Benjamin McEvoy <mcevoybenj@ecs.vuw.ac.nz>
2023-09-27 00:08:59 +1300

    placed cursor for all pages

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] public Texture2D cursorTexture;
+    [SerializeField] private Texture2D cursorTexture;
+    //[SerializeField] private int frameCount;
+    //[SerializeField ]private float frameRate;
+
+   // private int currentFrame;
+   // private float frameTimer;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.SetCursor(cursorTexture, new Vector2(10,10), CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        /*   if (Input.GetMouseButtonDown(0) == true)
+           {
+               for (int i = 0; i < cursorTexture.Length; i++)
+               {
+
+                   Cursor.SetCursor(cursorTexture[i], new Vector2(10,10), CursorMode.Auto);
+               }
+           }
+       }*/
     }
 
 }
