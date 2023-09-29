96d9a7ad1165b23853c2cea33765a7ff48e8fef4
Nathan Bennett <nathanbennett066@gmail.com>
2023-09-29 13:36:24 +1300

    Cleaned up FogController

--- Assets/Scripts/TileControllers/FogController.cs
+++ Assets/Scripts/TileControllers/FogController.cs
@@ -8,21 +8,11 @@ public class FogController : TileController
 {
     public Texture2D highOpacity;
     public Texture2D lowOpacity;
-    //public SpriteRenderer spriteRenderer;
-    //public Sprite highOpacity;
-    //public Sprite lowOpacity;
-
-    /*
-     * GameObject wall = Instantiate(wallTile, new Vector3(x * (cellSize + cellSpacing), y * (cellSize + cellSpacing), 1), Quaternion.identity, gameObject.transform);
-            wall.GetComponent<WallController>().Init(cellSize - cellSpacing * 2);
-            wall.GetComponent<WallController>().SetTexture(GetAdjacentControllers(x, y));
-     */
 
     private void Awake()
     {
         Sprite sprite = Sprite.Create(highOpacity, new Rect(0, 0, highOpacity.width, highOpacity.height), new Vector2(0.5f, 0.5f), textureSize);
         gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
-        //spriteRenderer.sprite = highOpacity;
     }
 
     public override bool CanEnter()
@@ -34,21 +24,12 @@ public class FogController : TileController
     {
         Sprite sprite = Sprite.Create(lowOpacity, new Rect(0, 0, lowOpacity.width, lowOpacity.height), new Vector2(0.5f, 0.5f), textureSize);
         gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
-        //spriteRenderer.sprite = lowOpacity;
-
     }
 
     public void raiseOpacity()
     {
         Sprite sprite = Sprite.Create(highOpacity, new Rect(0, 0, highOpacity.width, highOpacity.height), new Vector2(0.5f, 0.5f), textureSize);
         gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
-        //spriteRenderer.sprite = lowOpacity;
-
     }
-
-
-
-
-
 
 }
